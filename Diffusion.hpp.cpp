b5b042332206fe44c638bc0b24c3363aed5acaa5
Yaroslav Lisnyak <48571134+IaroslavLisniak@users.noreply.github.com>
2021-02-06 10:25:53 +0300

    Second commit

--- Diffusion.hpp.cpp
+++ Diffusion.hpp.cpp
@@ -133,7 +133,7 @@ namespace SiriusFM
 
 			double mu(double a_St, int a_t) const
 			{
-				return m_kappa * (m_theta - a_St); //Какой?
+				return m_mubar * a_St;
 			}
 
 			double sigma(double a_St, int a_t) const
